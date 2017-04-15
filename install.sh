c0284cc24480c4074b65571edc4ea6bd5581521e
Anthon Holmqvist <anthon.kendel@gmail.com>
2017-04-15 15:26:15 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+home=~/
+old_dotfiles="old_dotfiles"
+dotfiles=".bashrc .bash_profile .bash_aliases .profile .gitconfig"
+
+git_fullname=""
+git_email=""
+
+# Create dotfiles_old in homedir
+echo
+echo "Creating $old_dotfiles for backup of any existing dotfiles in $home."
+mkdir -p $home$old_dotfiles
+echo "Done"
+
+# Check if .gitconfig.local exist
+echo
+if [ ! -f $home".gitconfig.local" ]; then
+    echo "File .gitconfig.local not found, asking for settings."
+    echo "Please enter your full name: "
+    read -r git_fullname
+    echo "Please enter your email: "
+    read -r git_email
+
+    printf "[user]\n\tname=$git_fullname\n\temail=$git_email\n" > $home".gitconfig.local"
+fi
+
+echo
+for file in $dotfiles; do
+    echo "Moving old dotfiles in $home to $old_dotfiles."
+    mv $home$file $home$old_dotfiles
+    echo "Copying $file to $home."
+    cp $file $home
+done
+
+echo
+echo "Atom packages:"
+cat atom_packages
+read -p "Install atom_packages (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        apm install --packages-file atom_packages
+    ;;
+    * )
+    ;;
+esac
+
+echo
+echo "Please restart your terminal."
